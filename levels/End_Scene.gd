797cafcd414abe5b28fca46dcefaa5201d0e6973
Pyshrub <thepyromancer2048@gmail.com>
2023-11-30 11:50:07 -0500

    Completed
    Completely Working

--- levels/End_Scene.gd
+++ levels/End_Scene.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://levels/end_game.tscn")
