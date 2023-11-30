0046ce5e78580ce00a889e6038afe720b356ab42
anajmal <adilnajmal123@gmail.com>
2023-11-30 08:05:12 -0500

    Dialogue Update

---
+++ Dialogues/Dialog2Box.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var dialogue = [
+
+]
+
+var dialogue_index = 0
+var finished = false
+
+
+
+func _read():
+	load_dialogue()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		load_dialogue()
+func load_dialogue():
+	if dialogue_index < dialogue.size():
+		#$RichTextLabel.bbcode_text = dialogue1[dialogue_index] (IGNORE THIS LINE BUT DO NOT DELETE)
+		$Label.text = dialogue[dialogue_index]
+	else:
+		queue_free()
+	dialogue_index += 1
---
+++ Dialogues/Dialogue1Box.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var dialogue = [
+	'Where am I????',
+	'Ugh..need to find my pills. Pretty sure I had them when sleeping',
+	'(Use E to interact, P to use pills, and F for Flashlight)'
+]
+
+var dialogue_index = 0
+var finished = false
+
+
+
+func _read():
+	load_dialogue()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		load_dialogue()
+func load_dialogue():
+	if dialogue_index < dialogue.size():
+		#$RichTextLabel.bbcode_text = dialogue1[dialogue_index] (IGNORE THIS LINE BUT DO NOT DELETE)
+		$Label.text = dialogue[dialogue_index]
+	else:
+		queue_free()
+	dialogue_index += 1
