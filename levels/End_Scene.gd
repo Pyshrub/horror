6f999606334e65bde981df61d76c7af38ec3e69c
Pyshrub <thepyromancer2048@gmail.com>
2023-11-30 11:39:09 -0500

    ending
    let's go

--- levels/End_Scene.gd
+++ levels/End_Scene.gd
@@ -4,8 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.end = true
-
-
+	$AnimatedSprite2D2.play()
+	$AnimatedSprite2D3.play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
